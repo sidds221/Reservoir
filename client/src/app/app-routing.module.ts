f79c11f5d4e92880810e57235244d646c1890f29
Aashish Radhakrishnan <Aashish_Radhakrishnan@student.uml.edu>
2017-10-15 21:42:45 -0400

    Implemented Login Page
    Login and Register successfully implemented

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,22 +3,38 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
 // Our Array of Angular 2 Routes
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent // The Default Route
+    component: HomeComponent // Default Route
   },
   {
     path: 'dashboard',
-    component: DashboardComponent // The Dashboard Route
+    component: DashboardComponent, // Dashboard Route,
+    canActivate: [AuthGuard] // User must be logged in to view this route
   },
   {
     path: 'register',
-    component: RegisterComponent // The Register Route
+    component: RegisterComponent, // Register Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
   },
-  { path: '**', component: HomeComponent } // The "Catch-All" Route
+  {
+    path: 'login',
+    component: LoginComponent, // Login Route
+    canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent, // Profile Route
+    canActivate: [AuthGuard] // User must be logged in to view this route
+  },
+  { path: '**', component: HomeComponent } // "Catch-All" Route
 ];
 
 @NgModule({
